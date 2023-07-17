a27f4735f3d3ce98ace06456dbf017c1309f575a
Amy Rewoldt <rewoldt@csrlap2022-0130.local>
2023-07-17 12:38:10 -0400

    changed to get_geolength.py

--- get_geolength.py
+++ get_geolength.py
@@ -1,38 +1,80 @@
-#!/usr/bin/env python
+#!/usr/bin/env python                                                                                                       
 
+'''                                                                                                                          
+Some simple tools to fit ray traces and calculate geoeffective length                                                        
 '''
-Some simple tools to fit ray traces and calculate geoeffective length
-'''
+
 import numpy as np
 import matplotlib.pyplot as plt
 import reconx
-from scipy.stats import linregress
 
-ray_neg_3 = reconx.read_nulls('/Users/rewoldt/RECONX/run/null_line_neg_n03_t00160000.dat')
-ray_pls_3 = reconx.read_nulls('/Users/rewoldt/RECONX/run/null_line_pls_n03_t00160000.dat')
+def cross_product(u,v):  
+    dim = len(u)
+    s = []
+    for i in range(dim):
+        if i == 0:
+            j,k = 1,2
+            s.append(u[j]*v[k] - u[k]*v[j])
+        elif i == 1:
+            j,k = 2,0
+            s.append(u[j]*v[k] - u[k]*v[j])
+        else:
+            j,k = 0,1
+            s.append(u[j]*v[k] - u[k]*v[j])
+    return s
 
-def fit_ray(R,Z):
-     fit = linregress(R, Z)
-     i=0
-     while fit.rvalue < 0.999:
-          fit = linregress(R[i:], Z[i:])
-          i += 1
-     return fit
-     
-Xn = ray_neg_3['X']
-Yn = ray_neg_3['Y']
-Rn = np.sqrt(Xn**2+Yn**2)
-Zn = ray_neg_3['Z']
+def dot_product(u,v):
+    return sum([un*vn for un,vn in zip(u,v)])
 
-Xp = ray_pls_3['X']
-Yp = ray_pls_3['Y']
-Rp = np.sqrt(Xp**2+Yp**2)
-Zp = ray_pls_3['Z']
+def calc_uxb(u,B):
+    uxb = cross_product(u,B)
+    return uxb
 
-fit_neg = fit_ray(Rn,Zn)
-fit_pls = fit_ray(Rp,Zp)
+def get_potential(null_file_neg, null_file_pos):
 
-plt.plot(Rn,Zn,Rp,Zp)
-plt.plot(Rn, fit_neg.intercept + Rn*fit_neg.slope)
-plt.plot(Rn, fit_pls.intercept + Rn*fit_pls.slope)
-plt.show()
+    Xn = null_file_neg['X'][:200]
+    Yn = null_file_neg['Y'][:200]
+    Zn = null_file_neg['Z'][:200]
+    Xp = null_file_pos['X'][:200]
+    Yp = null_file_pos['Y'][:200]
+    Zp = null_file_pos['Z'][:200]
+
+    RE2km = 6378.14*(10**3) #m
+
+    geo_potential = []
+    dist = []
+    length = []
+
+    uxb = calc_uxb(u,B)
+
+    for i in range(len(Xn)):
+        for j in range(len(Xp)):
+            dist.append(np.sqrt((Xn[i]-Xp[j])**2 + (Yn[i]-Yp[j])**2 + (Zn[i]-Zp[j])**2)*RE2km)
+            L = (Xn[i]-Xp[j])*RE2km, (Yn[i]-Yp[j])*RE2km, (Zn[i]-Zp[j])*RE2km
+            length.append([(Xn[i]-Xp[j])*RE2km, (Yn[i]-Yp[j])*RE2km, (Zn[i]-Zp[j])*RE2km])
+            print (L, uxb)
+            print (np.dot(uxb,L), dot_product(uxb,L))
+            geo_potential.append(dot_product(uxb,L))
+
+    return dist, min(geo_potential)
+
+
+if __name__ == '__main__':
+    ray_neg = reconx.read_nulls('/Users/rewoldt/RECONX/run/null_line_neg_s03_111_t00160000.dat')
+    ray_pls = reconx.read_nulls('/Users/rewoldt/RECONX/run/null_line_pls_s03_125_t00160000.dat')
+
+    ux = -450*(10**3) #km/s
+    uy = 0
+    uz = 0
+
+    Bx = 0
+    By = 2*(10**(-9)) #nT
+    Bz = -10*(10**(-9)) #nT
+
+    B = [Bx, By, Bz]
+    u = [ux,uy,uz]
+
+    dist, geo_potential = get_potential(ray_neg, ray_pls)
+
+    print ('geo length: ', min(dist), 'geo potential', geo_potential, 'Should be getting ~95.72')
+
